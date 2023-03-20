d01381f93dd66739ba62a4a0a514d7f32a3e463f
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 15:00:22 +0100

    setup exo16

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-console.log(exo15(1234));
-function exo15(numbers) {
-  const numbersString = numbers.toString();
-  const splitedNumbers = numbersString.split("");
-  let res = 0;
-  for (let index = 0; index < splitedNumbers.length; index++) {
-    const number = parseInt(splitedNumbers[index]);
-    res += number;
-  }
+console.log(exo16([1, 2, 3, 4, 8]));
 
-  return res;
+/**
+ *
+ * @param {Array<number>} t
+ */
+function exo16(t) {
+  const firstItem = t[0];
+  const endItem = t[t.length - 1];
+  const newTable = t;
+  newTable.shift();
+  newTable.pop();
+  newTable.unshift(endItem);
+  newTable.push(firstItem);
+  return newTable;
 }
