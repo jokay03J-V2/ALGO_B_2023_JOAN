582885114712efbc719789dbb6c79c9c6e3225a4
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 14:11:35 +0100

    setup exo11

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-console.log(exo10([1, 3]));
-function exo10(t) {
+console.log(exo11([10, 20]));
+function exo11(t) {
   let res = 0;
   for (let index = 0; index < t.length; index++) {
     const number = t[index];
     res += number;
   }
-  return res;
+  return res / t.length;
 }
