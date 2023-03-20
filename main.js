621081ea6661cab0837390c7f14a7ed40d25687f
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 13:47:14 +0100

    setup exo3

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ exo2();
 
 function exo2() {
   let res = "";
-  for (let index = 1; index <= 10; index++) {
-    res += ` ${index}`;
+  for (let index = 1; index <= 100; index++) {
+    if (index % 2 !== 0) {
+      res += ` ${index}`;
+    }
   }
 
   console.log(res.slice(1, res.length));
