4615e15d6ff2daa57ccd901d58fe52a66170566e
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 15:18:57 +0100

    fix invalid positif number

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function exo12(t) {
   let res = [];
   for (let index = 0; index < t.length; index++) {
     const number = t[index];
-    if (number >= 0) res.push(number);
+    if (number > 0) res.push(number);
   }
   return res;
 }
