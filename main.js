d9a43a1a4372925f1ef84ef793557a7e5a005fcb
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 15:50:21 +0100

    remove useless code

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 console.log(exo15(1234));
 function exo15(numbers) {
   const numbersString = numbers.toString();
-  const splitedNumbers = numbersString.split("");
   let res = 0;
-  for (let index = 0; index < splitedNumbers.length; index++) {
-    const number = parseInt(splitedNumbers[index]);
+  for (let index = 0; index < numbersString.length; index++) {
+    const number = parseInt(numbersString[index]);
     res += number;
   }
 
