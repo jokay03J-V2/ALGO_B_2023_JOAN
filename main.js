c73dd738f10e54b9e95186ae0382c01c906fa453
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 16:27:12 +0100

    fix parse int radix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function exo15(numbers) {
   const numbersString = numbers.toString();
   let res = 0;
   for (let index = 0; index < numbersString.length; index++) {
-    const number = parseInt(numbersString[index]);
+    const number = parseInt(numbersString[index], 10);
     res += number;
   }
 
