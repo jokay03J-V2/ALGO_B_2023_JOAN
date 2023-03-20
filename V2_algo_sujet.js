2e22ac600f5613a099d076920c076fea05bcc95c
jokay03J-V2 <jmagro@edenschool.fr>
2023-03-20 13:40:45 +0100

    exo1

---
+++ V2_algo_sujet.js
@@ -0,0 +1,161 @@
+/*
+      EXO 1
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir les nombres de 1 a 10 inclus
+      Sans espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+ */
+
+
+/*
+      EXO 2
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir les nombres de 1 a 10 inclus
+      Avec des espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+ */
+
+
+/*
+      EXO 3
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir les nombres impaires inférieurs a 100
+      Avec des espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+ */
+
+
+/*
+      EXO 4
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir la table de multiplication de 7
+      Avec des espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+ */
+
+
+/*
+      EXO 5
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir toutes les tables de multiplication
+      de 1 a 10
+      Avec des espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+ */
+
+
+/*
+      EXO 6
+      Construire un nombre en utilisant une boucle
+      ce nombre devra être la somme des nombres de 1 a 10
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 7
+      Construire un nombre en utilisant une boucle
+      ce nombre devra être le factoriel du nombre 10
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 8
+      Construire un nombre en utilisant une boucle
+      ce nombre devra être la somme des nombres paires de 10 a 30
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 9
+      Écrire une fonction qui convertit une temperature de Celsius a Fahrenheit
+      La fonction prend un paramètre temperature (en Celsius)
+      La fonction doit retourner la température en Fahrenheit
+      ex: (30 °C × 9/5) + 32 = 86 °F
+ */
+
+
+/*
+      EXO 10
+      Écrire une fonction qui calcule la somme d'un tableau de nombres
+      La fonction prend un paramètre numbers qui est un tableau de taille minimum 1
+      La fonction doit retourner le résultat
+      Vous devez utiliser une boucle (pas de reduce)
+ */
+
+
+/*
+      EXO 11
+      Écrire une fonction qui calcule la moyenne d'un tableau de nombres
+      La fonction prend un paramètre numbers qui est un tableau de taille minimum 1
+      La fonction doit retourner le résultat
+      Vous devez utiliser une boucle (pas de reduce)
+ */
+
+
+/*
+      EXO 12
+      Écrire une fonction qui prend un tableau de nombres en paramètre (numbers)
+      Et retourne un nouveau tableau contenant uniquement les nombres positifs
+      La fonction doit retourner le nouveau tableau
+      Vous devez utiliser une boucle (pas de filter)
+ */
+
+
+/*
+      EXO 13
+      Écrire une fonction qui prend un tableau de nombres en paramètre (numbers)
+      Et retourne le nombre maximum dans ce tableau
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 14
+      Construire une chaîne de caractères en utilisant une boucle
+      Cette chaîne de caractères devra contenir les 10 premiers nombres de la suite
+      de fibonacci
+      Avec des espaces entre les nombres
+      Ne pas retourner la chaîne de caractères, affichez la avec un console.log()
+      Tips: Fibonacci => 0 1 1 2 3 (2+1) 5 (3+2)... n (n-2 + n-1)
+ */
+
+
+/*
+      EXO 15
+      Écrire une fonction qui prend un nombre entier positif en paramètre (number)
+      Et retourne la somme des chiffres qui composent ce nombre
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 16
+      Écrire une fonction qui prend un tableau de nombres de taille minimum 2 en
+      paramètre (numbers)
+      Et retourne le même tableau en inversant le premier et le dernier élément
+      La fonction doit retourner le tableau
+ */
+
+
+
+/*
+      EXO 17
+      Écrire une fonction qui prend une chaîne de caractères en paramètre (text)
+      Et retourne le nombre de caractères spéciaux contenus dans ce texte
+      § ! @ # $ % * ( ) _ - = + { } [ ] \ | < > ? / . , ; sont les seuls caractères      spéciaux que vous pourrez trouvez dans le texte
+      La fonction doit retourner le résultat
+ */
+
+
+/*
+      EXO 18
+      Écrire une fonction qui prend une chaîne de caractères en paramètre (text)
+      text contient des nombres séparés par des virgules
+      la fonction doit calculer la moyenne des nombres présents dans cette chaîne de      caractères
+      La fonction doit retourner le résultat
+ */
+
+
---
+++ main.js
@@ -0,0 +1,11 @@
+exo1();
+
+function exo1() {
+  let res = "";
+  for (let index = 1; index <= 10; index++) {
+    res += index;
+  }
+
+  console.log(res);
+}
+
